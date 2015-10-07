81b38d606fcc60b7d72f01224fb337299dd8780c
Steve Hogg <steve@collecto.io>
2015-10-07 16:45:08 +1300

    Gulp watch files and auto-refresh browser

--- gulp/gulp.config.js
+++ gulp/gulp.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     build_dir: 'build',
     app_files: {
+        html: ['src/**/*.html'],
+        js: ['src/**/*.js'],
         tpl_src: ['./build/app/**/*.js',
             './build/assets/css/**/*.css'],
         vendor_src: ['rx.min.js',
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
     del = require('del'),
     runSequence = require('run-sequence'),
     eventStream = require('event-stream'),
@@ -7,7 +8,7 @@ var gulp = require('gulp'),
 var config = require('./gulp/gulp.config.js');
 
 gulp.task('default', function() {
-    runSequence('build');
+    runSequence('build', 'watch');
 });
 
 gulp.task('build', function(callback) {
@@ -20,7 +21,8 @@ gulp.task('copy-injected-index', function() {
    return gulp.src('./src/**/*.html')
        .pipe(inject(eventStream.merge(gulp.src(config.app_files.tpl_src), gulp.src(config.app_files.vendor_src)),
            {ignorePath: 'build'}))
-       .pipe(gulp.dest(config.build_dir));
+       .pipe(gulp.dest(config.build_dir))
+       .pipe(browserSync.stream());
 });
 
 gulp.task('copy-app-html', function() {
@@ -44,5 +46,15 @@ gulp.task('copy-vendor-js', function() {
 });
 
 gulp.task('clean', function() {
-   return del([config.build_dir], {force: true});
+    return del([config.build_dir], {force: true});
+});
+
+gulp.task('watch', function() {
+    gulp.watch([config.app_files.html, config.app_files.js], ['build']);
+
+    browserSync.init({
+        browser: "google chrome",
+        server: config.build_dir,
+        logFileChanges: false
+    });
 });
